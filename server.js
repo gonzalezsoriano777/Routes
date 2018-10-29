0b16c50a937c836861c95cd9e688d3d6c7457ab0
Cliff <tools@3csonline.com>
2018-10-29 15:38:57 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json())
+
+const db = require('./config/keys').mongoURI;
+
+mongoose
+    .connect(db)
+    .then(() => console.log("MongoDB Connected"))
+    .catch(err => console.log(err))
+
+app.get('/', (req, res) => res.send('Hello'))
+
+const port = process.env.PORT || 5000
+
+app.listen(port, () =>console.log('Application listening on port', port))
