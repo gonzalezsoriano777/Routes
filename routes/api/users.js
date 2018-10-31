85794fbdeb32c2b64a89affcba3f2b3f92255b28
Hector Gonzalez <raftb.gonzalez@outlook.com>
2018-10-31 00:11:12 +0000

    Practice

--- routes/api/users.js
+++ routes/api/users.js
@@ -30,4 +30,44 @@ router.get('/:name', (req, res) => {
      .catch(err => res.status(500).json({message: err}));
 })
 
+router.post('/', (req, res) => {
+    const { name, password, avatar } = req.body;
+    
+    const newUser = new User({
+        name,
+        password, 
+        avatar
+    })
+    
+    newUser.save()
+     .then(user => res.status(201).json(user)) // 201 status code means you have added a user or anything in general
+      .catch(err => {
+          res.status(500).json({ message: err }); // 500 status code means there is an error of the user or something being added from the code
+    });
+});
+
+
+router.delete('/:name', (req, res) => {
+    // Todo: protected route ensure the user is the one deleting
+    const { name, password, avatar } = req.params;
+    User.findOne({ name })
+     .then(user => {
+         if(!user) {
+             return res.status(404).json({message: `User: ${name} not found`}); // 404 status code meaning, that the user is non existent
+         }
+         user.remove()
+          .then(() => res.status(204).json({message: `User ${name} successfully deleted`})) // 204 status code meaning,  
+          .catch(err => res.status(500).json({err}));
+     })
+     .catch(err => res.status(500).json({message: err}));
+});
+
+router.put('/:name', (req, res) => {
+    
+  //  findOne()
+    //  .then(user) => {
+     //     findOneAndUpdate()
+      
+})
+
 module.exports = router;
